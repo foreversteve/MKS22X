b76c5a410af462198d67dae573d5a7f7c3c957c6
Steven Liu <hliu4@stuy.edu>
2018-05-09 18:44:15 -0400

    Fixed Last Bugs

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,5 +1,4 @@
 
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,7 +38,7 @@ public class MyHeap <T extends Comparable<T>>{
 		T temp = data[0];
 		data[0] = data[size-1];
 
-		System.out.println(this);
+		//System.out.println(this);
 		data[size-1] = null;
 		size -= 1;
 
@@ -55,7 +54,7 @@ public class MyHeap <T extends Comparable<T>>{
 		return size;
 	}
 
- @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	private void resize() {
 		T[] temp = (T[])new Comparable[size*2];
 		for (int i = 0; i < size; i++) {
@@ -107,9 +106,9 @@ public class MyHeap <T extends Comparable<T>>{
 	private void moveDown() {
 
 		int place = 0;
-		while (place*2+2 < size) {
+		while (place*2+1 < size) {
 			if (type) {
-				if (data[place*2+1].compareTo(data[place*2+2]) < 0) {
+				if (place*2+2 < size && data[place*2+1].compareTo(data[place*2+2]) < 0) {
 					if (data[place].compareTo(data[place*2+2]) < 0 ) {
 						T temp = data[place];
 						data[place] = data[place*2+2];
@@ -122,7 +121,7 @@ public class MyHeap <T extends Comparable<T>>{
 					}
 				}
 				else {
-					if (data[place].compareTo(data[place*2+1]) < 0 ) {
+					if (place*2+1 < size && data[place].compareTo(data[place*2+1]) < 0 ) {
 						T temp = data[place];
 						data[place] = data[place*2+1];
 						data[place*2+1] = temp;
@@ -136,7 +135,7 @@ public class MyHeap <T extends Comparable<T>>{
 
 			}
 			else {
-				if (data[place*2+1].compareTo(data[place*2+2]) < 0) {
+				if (place*2 + 2 < size && data[place*2+1].compareTo(data[place*2+2]) < 0) {
 					if (data[place].compareTo(data[place*2+1]) > 0 ) {
 						T temp = data[place];
 						data[place] = data[place*2+1];
@@ -149,7 +148,7 @@ public class MyHeap <T extends Comparable<T>>{
 					}
 				}
 				else {
-					if (data[place].compareTo(data[place*2+1]) > 0 ) {
+					if (place*2+2 < size && data[place].compareTo(data[place*2+2]) > 0 ) {
 						T temp = data[place];
 						data[place] = data[place*2+2];
 						data[place*2+2] = temp;
@@ -166,49 +165,64 @@ public class MyHeap <T extends Comparable<T>>{
 		}
 
 	}
+	/*
+	private void moveDown(int place) {
+		T temp1 = data[place+1];
+		if (place*2+1 < size) {
 
+		}
+		if (place*2+1 < size) {
+
+		}
+	}
+	*/
 	public static void main(String[] args) {
-
-		MyHeap<Integer> a = new MyHeap<>(false);
+		/*
+		MyHeap<Integer> a = new MyHeap<>(true);
 		for (int i = 0 ; i < 10; i++) {
-			a.add(i);
+			a.add((int) (Math.random()*100));
 		}
 		//a.test();
 		System.out.println(a);
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 8; i++) {
 			 System.out.println(a.remove());
-			 System.out.println(a);
-			 System.out.println();
+			 //System.out.println(a);
+			 //System.out.println();
 		}
 		System.out.println(a);
-
-		/*
-		MyHeap a = new MyHeap(false);
-	    String[] b = new String[10];
-	    for(int i = 0; i < 10; i++){
-	      int temp = (int)(Math.random() * 26) + 97;
-	      char value = (char)temp;
-	      a.add("" + value);
-	      b[i] = "" + value;
-	    }
-
-	    Arrays.sort(b);
-
-	    System.out.println("MyHeap: " + a);
-	    System.out.println("Arrays: "+ Arrays.toString(b));
-
-	    for(int i = 0; i < 10; i++){
-	      //System.out.println("size: " + a.size());
-	      //System.out.println("heap before: " + a.toString());
-	      String temp = a.remove()+"";
-	      if(!(temp.equals(b[i]))){
-	        System.out.println("there is an error");
-	        System.out.println(temp);
-	        System.out.println(b[i]);
-	        System.out.println(a);
-	      }
-	    }
-
 		*/
+		MyHeap<Integer> a = new MyHeap<>(false);
+
+		ArrayList<Integer> test = new ArrayList<>();
+		for (int i = 0 ; i < 100; i++) {
+			int temp = (int) (Math.random()*100);
+			a.add(temp);
+			test.add(temp);
+		}
+		test.sort(null);
+
+		//System.out.println(a);
+		//System.out.println(test);
+
+		//System.out.println();
+
+		for (int i = 0; i < 75; i++) {
+			//System.out.println(a);
+			int temp1 = a.remove();
+			int temp2 = test.get(0);
+			test.remove(0);
+			if (temp1 != temp2) {
+				System.out.println("Error found");
+			}
+			//System.out.println(temp1 + " " + temp2);
+			//System.out.println(a);
+			//System.out.println(test);
+
+			//System.out.println();
+		}
+
+
+
+
 	}
 }
